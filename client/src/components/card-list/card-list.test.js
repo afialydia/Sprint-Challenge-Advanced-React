6b4c328fb1aa625e1bbf0eab433a261767b68e50
Afia-Lydia <you@example.com>
2019-11-01 14:26:05 -0400

    added test to card list

--- client/src/components/card-list/card-list.test.js
+++ client/src/components/card-list/card-list.test.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import {render, cleanup} from '@testing-library/react'
-import 'jest-dom/extend-expect'
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import Cardlist from "./card-list.component";
 
 //test that props are being passed down
 //test that info is being mapped over
-afterEach(cleanup)
+afterEach(cleanup);
 
-test('renders "Server disconnected... please try again', ()=>{
-    const {getByText} = render(<Cardlist />)
-    expect(getByText(/Server Disconnected... Please try again/i)).toBeInTheDocument()
-})
+test('renders "Server disconnected... please try again', () => {
+	const { getByText } = render(<Cardlist />);
+	expect(
+		getByText(/Server Disconnected... Please try again/i)
+	).toBeInTheDocument();
+});
+
+
+test("renders athletes", () => {
+	const fakeAthletes = [
+		{ name: "Carli Lloyd", country: "United States", searches: 9 },
+		{ name: "Julie Ertz", country: "United States", searches: 8 }
+	];
+	render(<Cardlist athletes={fakeAthletes} />);
+});
